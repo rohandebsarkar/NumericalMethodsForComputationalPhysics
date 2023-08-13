e4c0f8a7dd4857fc6b72ce7e4aa857907a9b8e3e
Rohan Deb Sarkar <rohandebsarkar@protonmail.com>
2023-08-13 11:54:02 +0530

    Add Quantum Mechanics

--- scripts/jupyter_notebook_to_latex.py
+++ scripts/jupyter_notebook_to_latex.py
@@ -48,8 +48,8 @@ def jupyter_notebook_to_latex(path_to_jupyter_notebooks, path_to_latex_files, pa
     for i in latex_files:
         delete_files(i, path_to_jupyter_notebooks)
 
-path_to_jupyter_notebooks = "projects/undergraduate/Mathematical_Physics_II/JupyterNotebook"
-path_to_latex_files = "projects/undergraduate/Mathematical_Physics_II/LaTeX/input/JupyterNotebook"
-path_to_main_latex_file = "projects/undergraduate/Mathematical_Physics_II/LaTeX"
+path_to_jupyter_notebooks = "projects/undergraduate/Quantum_Mechanics/JupyterNotebook"
+path_to_latex_files = "projects/undergraduate/Quantum_Mechanics/LaTeX/input/JupyterNotebook"
+path_to_main_latex_file = "projects/undergraduate/Quantum_Mechanics/LaTeX"
 
 jupyter_notebook_to_latex(path_to_jupyter_notebooks, path_to_latex_files, path_to_main_latex_file)
